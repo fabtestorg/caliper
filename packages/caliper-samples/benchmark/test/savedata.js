0255ea8cfddf4c0cf19da29a3398b02cee6588fd
yunfeng <zyf7862634@163.com>
2019-06-24 13:14:46 +0800

    feat - add ex02

---
+++ packages/caliper-samples/benchmark/test/savedata.js
@@ -0,0 +1,66 @@
+/*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+'use strict';
+
+module.exports.info  = 'savedata 2k data';
+
+let account_array = [];
+let txnPerBatch;
+let bc, contx;
+module.exports.init = function(blockchain, context, args) {
+    if(!args.hasOwnProperty('txnPerBatch')) {
+        args.txnPerBatch = 1;
+    }
+    txnPerBatch = args.txnPerBatch;
+    bc = blockchain;
+    contx = context;
+
+    return Promise.resolve();
+};
+
+/**
+ * Generates simple workload
+ * @returns {Object} array of json objects
+ */
+function generateWorkload() {
+    let workload = [];
+    for(let i= 0; i < txnPerBatch; i++) {
+        if (bc.bcType === 'fabric-ccp') {
+            workload.push({
+                chaincodeFunction: 'savedata',
+                chaincodeArguments: ["gakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdddddddddddasdfdsfdsfsdfaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafdaabcacacagakgjdsahfdsafd"],
+            });
+        } else {
+            console.log("**********bcType not support*******")
+            // workload.push({
+            //     'verb': 'open',
+            //     'account': acc_id,
+            //     'money': initMoney
+            // });
+        }
+    }
+    return workload;
+}
+
+module.exports.run = function() {
+    let args = generateWorkload();
+    return bc.invokeSmartContract(contx, 'simple', 'v0', args, 100);
+};
+
+module.exports.end = function() {
+    return Promise.resolve();
+};
+
+module.exports.account_array = account_array;
